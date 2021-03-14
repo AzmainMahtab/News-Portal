d11b55437dc764ac0d9fd3bb70440e47cbc5986c
Azmain Mahtab <azmainmahtab97@gmail.com>
2021-03-14 20:16:30 +0600

    Linkes and APIs are all working

--- portal_app/urls.py
+++ portal_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('youtube/', views.youtube),
+    path('', views.index, name='home'),
+    path('youtube/', views.youtube, name='youtube'),
+    path('game/', views.game, name='game'),
+    path('shop/', views.shop, name='shop'),
+    path('trends/', views.trends, name='trends')
 ]
--- portal_app/views.py
+++ portal_app/views.py
@@ -58,3 +58,58 @@ def youtube(request):
     
 
     return render(request,"portal_app/youtube.html", {"videos": videos})
+
+def game(request):
+    return render(request,"portal_app/game.html")
+
+def shop(request):
+    return render(request,"portal_app/shop.html")
+
+def trends(request):
+    req_link = 'https://www.googleapis.com/youtube/v3/videos'
+    #Parameters for the API
+    params = {
+        'key': settings.YOUTUBE_DATA_API_KEY,
+        'part': 'snippet, contentDetails',
+        'chart': 'mostPopular',
+        'maxResults': 5
+    }
+    r = requests.get(req_link,params= params).json()
+    #items for itarating item int the json list
+    items = r['items']
+    #empty dictonaries to append to
+    #video_ids = []
+    titles = []
+    urls = []
+    durations = []
+    thumbnails = []
+    #itarating items and appending them to dictonaries
+    for i in range(len(items)):
+        video = items[i]
+        #video_ids.append(video['id'])
+        titles.append(video['snippet']['title'])
+        urls.append(f'https://www.youtube.com/watch?v={ video["id"] }')
+        #pasing the duration to mins
+        durations.append(int(parse_duration(video['contentDetails']['duration']).total_seconds()//60))
+        thumbnails.append(video['snippet']['thumbnails']['high']['url'])
+    
+    videos = zip(thumbnails, titles, durations, urls)
+
+    newsApi = NewsApiClient(api_key = 'ede03d90771a44f0b747d38ba3d8340a')
+    headLines = newsApi.get_top_headlines(sources = 'abc-news')
+    articles = headLines['articles']
+    title = []
+    dis = []
+    image = []
+    link = []
+
+    for i in range(len(articles)):
+        article = articles[i]
+        title.append(article['title'])
+        dis.append(article['description'])
+        image.append(article['urlToImage'])
+        link.append(article['url'])
+
+    myList = zip(title, dis, image, link)  
+
+    return render(request,"portal_app/trends.html", {"videos": videos, "myList": myList})
